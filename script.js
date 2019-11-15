62bc7ec87fb8aae58f9d3433814fef9de504e624
JonathanDahlgren <jonis1994@live.se>
2019-11-15 14:53:01 +0100

    after help from teatcher

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+window.onload = loadPage
+
+function loadPage() {
+    updateAge()
+}
+
+function updateAge() {
+    const now = new Date().getFullYear()
+    const birth = new Date('1994-04-25').getFullYear()
+
+    console.log(now - birth)
+}
 
 function omMig()  {
-    document.getElementById("aboutMe").innerHTML = "afafadadadadadad";
+    const aboutMe = document.getElementById("aboutMe");
+
+    if (!aboutMe.style.maxHeight) {
+        aboutMe.style.maxHeight = '10rem';
+    } else {
+        aboutMe.style.maxHeight = null;
+    }
+        
 }
